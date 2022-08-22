e0433894a50d342f881dd947e9c299188242d271
xfqwdsj <xfqwdsj@qq.com>
2022-08-22 16:45:12 +0800

    fix: 恢复误删除的文件

---
+++ app/src/main/java/xyz/xfqlittlefan/notdeveloper/Preference.kt
@@ -0,0 +1,5 @@
+package xyz.xfqlittlefan.notdeveloper
+
+const val DEVELOPMENT_SETTINGS_ENABLED = "development_settings_enabled"
+const val ADB_ENABLED = "adb_enabled"
+const val ADB_WIFI_ENABLED = "adb_wifi_enabled"
