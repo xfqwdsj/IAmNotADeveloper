c919bff0b56541bfe2d129bde1c1bf982cc2aa83
xfqwdsj <xfqwdsj@qq.com>
2023-10-04 21:45:10 +0800

    refactor: clean code

--- app/src/main/java/xyz/xfqlittlefan/notdeveloper/xposed/Hook.kt
+++ app/src/main/java/xyz/xfqlittlefan/notdeveloper/xposed/Hook.kt
@@ -2,13 +2,15 @@ package xyz.xfqlittlefan.notdeveloper.xposed
 
 import android.content.ContentResolver
 import android.provider.Settings
+import android.util.Log
 import androidx.annotation.Keep
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XC_MethodHook.MethodHookParam
 import de.robv.android.xposed.XSharedPreferences
 import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
-import de.robv.android.xposed.callbacks.XC_LoadPackage
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 import xyz.xfqlittlefan.notdeveloper.ADB_ENABLED
 import xyz.xfqlittlefan.notdeveloper.ADB_WIFI_ENABLED
 import xyz.xfqlittlefan.notdeveloper.BuildConfig
@@ -16,181 +18,165 @@ import xyz.xfqlittlefan.notdeveloper.DEVELOPMENT_SETTINGS_ENABLED
 
 @Keep
 class Hook : IXposedHookLoadPackage {
-    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+    private val tag = "NotDeveloper"
+
+    override fun handleLoadPackage(lpparam: LoadPackageParam) {
         if (lpparam.packageName.startsWith("android") || lpparam.packageName.startsWith("com.android")) {
             return
         }
 
+        Log.i(tag, "processing " + lpparam.packageName)
+
         if (lpparam.packageName == BuildConfig.APPLICATION_ID) {
-            XposedHelpers.findAndHookMethod("xyz.xfqlittlefan.notdeveloper.xposed.ModuleStatusKt",
+            XposedHelpers.findAndHookMethod(
+                "xyz.xfqlittlefan.notdeveloper.xposed.ModuleStatusKt",
                 lpparam.classLoader,
                 "isModuleActive",
                 object : XC_MethodHook() {
                     override fun beforeHookedMethod(param: MethodHookParam) {
                         param.result = true
                     }
-                })
+                },
+            )
         }
 
-        val preferences = XSharedPreferences(BuildConfig.APPLICATION_ID)
+        val prefs = XSharedPreferences(BuildConfig.APPLICATION_ID)
 
-        XposedHelpers.findAndHookMethod(Settings.Global::class.java,
-            "getInt",
-            ContentResolver::class.java,
-            String::class.java,
-            Int::class.java,
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    preferences.reload()
-                    when {
-                        preferences.getBoolean(
-                            DEVELOPMENT_SETTINGS_ENABLED, true
-                        ) && param.args[1] == DEVELOPMENT_SETTINGS_ENABLED -> {
-                            param.result = 0
-                        }
-
-                        preferences.getBoolean(
-                            ADB_ENABLED, true
-                        ) && param.args[1] == ADB_ENABLED -> {
-                            param.result = 0
-                        }
-
-                        preferences.getBoolean(
-                            ADB_WIFI_ENABLED, true
-                        ) && param.args[1] == ADB_WIFI_ENABLED -> {
-                            param.result = 0
-                        }
-                    }
-                }
-            })
-
-        XposedHelpers.findAndHookMethod(Settings.Global::class.java,
-            "getInt",
-            ContentResolver::class.java,
-            String::class.java,
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    preferences.reload()
-                    when {
-                        preferences.getBoolean(
-                            DEVELOPMENT_SETTINGS_ENABLED, true
-                        ) && param.args[1] == DEVELOPMENT_SETTINGS_ENABLED -> {
-                            param.result = 0
-                        }
-
-                        preferences.getBoolean(
-                            ADB_ENABLED, true
-                        ) && param.args[1] == ADB_ENABLED -> {
-                            param.result = 0
-                        }
-
-                        preferences.getBoolean(
-                            ADB_WIFI_ENABLED, true
-                        ) && param.args[1] == ADB_WIFI_ENABLED -> {
-                            param.result = 0
-                        }
-                    }
-                }
-            })
-
-        XposedHelpers.findAndHookMethod(Settings.Secure::class.java,
-            "getInt",
-            ContentResolver::class.java,
-            String::class.java,
-            Int::class.java,
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    preferences.reload()
-                    when {
-                        preferences.getBoolean(
-                            DEVELOPMENT_SETTINGS_ENABLED, true
-                        ) && param.args[1] == DEVELOPMENT_SETTINGS_ENABLED -> {
-                            param.result = 0
-                        }
-
-                        preferences.getBoolean(
-                            ADB_ENABLED, true
-                        ) && param.args[1] == ADB_ENABLED -> {
-                            param.result = 0
-                        }
-                    }
-                }
-            })
-
-        XposedHelpers.findAndHookMethod(Settings.Secure::class.java,
-            "getInt",
-            ContentResolver::class.java,
-            String::class.java,
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    preferences.reload()
-                    when {
-                        preferences.getBoolean(
-                            DEVELOPMENT_SETTINGS_ENABLED, true
-                        ) && param.args[1] == DEVELOPMENT_SETTINGS_ENABLED -> {
-                            param.result = 0
-                        }
-
-                        preferences.getBoolean(
-                            ADB_ENABLED, true
-                        ) && param.args[1] == ADB_ENABLED -> {
-                            param.result = 0
-                        }
-                    }
-                }
-            })
-
-        // Call the hideSystemProps function
-        hideSystemProps(lpparam)
-    }
-
-    // Define the hideSystemProps function
-    fun hideSystemProps(lpparam: XC_LoadPackage.LoadPackageParam) {
-        XposedBridge.log("LOAD " + lpparam.packageName)
-
-        val clazz = XposedHelpers.findClassIfExists(
-            "android.os.SystemProperties", lpparam.classLoader)
-
-        XposedBridge.log("CLASS " + clazz?.name)
-
-        if (clazz != null) {
-            val ffsReady = "sys.usb.ffs.ready";
-            val usbState = "sys.usb.state";
-            val usbConfig = "sys.usb.config";
-            val rebootFunc = "persist.sys.usb.reboot.func";
-            val methodGet = "get"
-            val methodGetBoolean = "getBoolean"
-            val methodGetInt = "getInt"
-            val methodGetLong = "getLong"
-            val overrideAdb = "mtp"
-
-            listOf(methodGet, methodGetBoolean, methodGetInt, methodGetLong).forEach {
-                XposedBridge.hookAllMethods(clazz, it,
-                    object : XC_MethodHook() {
-                        override fun afterHookedMethod(param: MethodHookParam?) {
-                            XposedBridge.log(
-                                "ACCESS " + param!!.method.name + " PARAM " + param.args[0])
-
-                            if (param.args[0] != ffsReady && param.method.name != methodGet) {
-                                return
-                            }
-
-                            when(param.args[0]) {
-                                ffsReady -> {
-                                    when(param.method.name) {
-                                        methodGet -> param.result = "0"
-                                        methodGetBoolean -> param.result = false
-                                        methodGetInt -> param.result = 0
-                                        methodGetLong -> param.result = 0L
-                                    }
-                                }
-                                usbState -> param.result = overrideAdb
-                                usbConfig -> param.result = overrideAdb
-                                rebootFunc -> param.result = overrideAdb
-                            }
-                        }
-                    })
+        val newApiCallback = object : XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                prefs.reload()
+                hookResultToZero(
+                    lpparam,
+                    prefs,
+                    param,
+                    DEVELOPMENT_SETTINGS_ENABLED,
+                    ADB_ENABLED,
+                    ADB_WIFI_ENABLED
+                )
             }
         }
+
+        val oldApiCallback = object : XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                prefs.reload()
+                hookResultToZero(lpparam, prefs, param, DEVELOPMENT_SETTINGS_ENABLED, ADB_ENABLED)
+            }
+        }
+
+        XposedHelpers.findAndHookMethod(
+            Settings.Global::class.java,
+            "getInt",
+            ContentResolver::class.java,
+            String::class.java,
+            Int::class.java,
+            newApiCallback,
+        )
+
+        XposedHelpers.findAndHookMethod(
+            Settings.Global::class.java,
+            "getInt",
+            ContentResolver::class.java,
+            String::class.java,
+            newApiCallback,
+        )
+
+        XposedHelpers.findAndHookMethod(
+            Settings.Secure::class.java,
+            "getInt",
+            ContentResolver::class.java,
+            String::class.java,
+            Int::class.java,
+            oldApiCallback,
+        )
+
+        XposedHelpers.findAndHookMethod(
+            Settings.Secure::class.java,
+            "getInt",
+            ContentResolver::class.java,
+            String::class.java,
+            oldApiCallback,
+        )
+
+        if (prefs.getBoolean(ADB_ENABLED, true)) {
+            hideSystemProps(lpparam)
+        }
+    }
+
+    private fun hideSystemProps(lpparam: LoadPackageParam) {
+        val clazz = XposedHelpers.findClassIfExists(
+            "android.os.SystemProperties", lpparam.classLoader
+        )
+
+        if (clazz == null) {
+            XposedBridge.log("$tag: cannot find SystemProperties class")
+            return
+        }
+
+        val ffsReady = "sys.usb.ffs.ready"
+        val usbState = "sys.usb.state"
+        val usbConfig = "sys.usb.config"
+        val rebootFunc = "persist.sys.usb.reboot.func"
+        val methodGet = "get"
+        val methodGetBoolean = "getBoolean"
+        val methodGetInt = "getInt"
+        val methodGetLong = "getLong"
+        val overrideAdb = "mtp"
+
+        listOf(methodGet, methodGetBoolean, methodGetInt, methodGetLong).forEach {
+            XposedBridge.hookAllMethods(
+                clazz, it,
+                object : XC_MethodHook() {
+                    override fun beforeHookedMethod(param: MethodHookParam) {
+                        val arg = param.args[0] as String
+                        Log.i(
+                            tag,
+                            "processing ${param.method.name} from ${lpparam.packageName} with arg $arg"
+                        )
+
+                        if (arg != ffsReady && param.method.name != methodGet) {
+                            XposedBridge.log("$tag: processed ${param.method.name} from ${lpparam.packageName} receiving invalid arg $arg")
+                            return
+                        }
+
+                        when (arg) {
+                            ffsReady -> {
+                                when (param.method.name) {
+                                    methodGet -> param.result = "0"
+                                    methodGetBoolean -> param.result = false
+                                    methodGetInt -> param.result = 0
+                                    methodGetLong -> param.result = 0L
+                                }
+                            }
+
+                            usbState -> param.result = overrideAdb
+                            usbConfig -> param.result = overrideAdb
+                            rebootFunc -> param.result = overrideAdb
+                        }
+
+                        Log.i(tag, "hooked ${param.method.name}($arg): ${param.result}")
+                    }
+                },
+            )
+        }
+    }
+
+    private fun hookResultToZero(
+        lpparam: LoadPackageParam,
+        preferences: XSharedPreferences,
+        param: MethodHookParam,
+        vararg keys: String
+    ) {
+        val arg = param.args[1] as String
+        Log.i(tag, "processing ${param.method.name} from ${lpparam.packageName} with arg $arg")
+
+        keys.forEach { key ->
+            if (preferences.getBoolean(key, true) && arg == key) {
+                param.result = 0
+                Log.i(tag, "hooked ${param.method.name}($arg): ${param.result}")
+                return
+            }
+        }
+
+        XposedBridge.log("$tag: processed ${param.method.name} without changing result")
     }
 }
