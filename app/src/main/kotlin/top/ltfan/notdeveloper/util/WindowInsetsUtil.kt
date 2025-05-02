cb4952c9a5abb6307e11e6b24611907a6e18fced
LTFan <xfqwdsj@qq.com>
2025-05-02 12:18:36 +0800

    refactor: remove WindowInsetsUtil.kt and its content

--- app/src/main/kotlin/top/ltfan/notdeveloper/util/WindowInsetsUtil.kt
+++
@@ -1,9 +0,0 @@
-package top.ltfan.notdeveloper.util
-
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
-
-operator fun WindowInsets.plus(other: WindowInsets): WindowInsets = add(other)
-
-val WindowInsets.Companion.allBars: WindowInsets
-    @Composable get() = WindowInsets.systemBars + WindowInsets.displayCutout.only(WindowInsetsSides.Horizontal)
