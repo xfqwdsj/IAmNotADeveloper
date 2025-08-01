f2ed90a91514d66337332a57c47bb484dfce4065
LTFan <xfqwdsj@qq.com>
2025-08-01 16:34:54 +0800

    refactor: 使用dsl以增加灵活性

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,5 +89,6 @@ dependencies {
     implementation(libs.compose.animation)
     implementation(libs.compose.material3)
     implementation(libs.preference)
+    implementation(libs.dslUtilities)
     compileOnly(libs.xposed.api)
 }
