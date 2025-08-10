667daa778d649afdd05f39f38f5331c731840b33
LTFan <xfqwdsj@qq.com>
2025-08-10 12:59:42 +0800

    refactor: 提取ListItemColors

--- app/src/main/kotlin/ui/util/Color.kt
+++ app/src/main/kotlin/ui/util/Color.kt
@@ -1,8 +1,13 @@
 package top.ltfan.notdeveloper.ui.util
 
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 val LowestCardColors @Composable inline get() =
     CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceContainerLowest)
+
+val TransparentListItemColors @Composable inline get() =
+    ListItemDefaults.colors(containerColor = Color.Transparent)
