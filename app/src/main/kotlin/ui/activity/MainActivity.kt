3f5b35925af7a787ab114f0d1756fd75d1e06a2d
LTFan <xfqwdsj@qq.com>
2025-08-12 22:34:36 +0800

    refactor: 更新ui结构

--- app/src/main/kotlin/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/ui/activity/MainActivity.kt
@@ -11,21 +11,31 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.navigation3.rememberListDetailSceneStrategy
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation3.ui.NavDisplay
 import top.ltfan.notdeveloper.service.systemService
 import top.ltfan.notdeveloper.ui.page.Main
 import top.ltfan.notdeveloper.ui.theme.IAmNotADeveloperTheme
 import top.ltfan.notdeveloper.ui.util.AppWindowInsets
+import top.ltfan.notdeveloper.ui.util.HazeZIndex
+import top.ltfan.notdeveloper.ui.util.hazeEffectBottom
+import top.ltfan.notdeveloper.ui.util.hazeSource
 import top.ltfan.notdeveloper.ui.util.only
+import top.ltfan.notdeveloper.ui.util.plus
 import top.ltfan.notdeveloper.ui.viewmodel.AppViewModel
 import top.ltfan.notdeveloper.util.isMiui
 import top.ltfan.notdeveloper.xposed.statusIsPreferencesReady
@@ -51,22 +61,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            IAmNotADeveloperTheme {
+            IAmNotADeveloperTheme(viewModel) {
                 val insets = AppWindowInsets
-                val navBarHeightFactor by animateFloatAsState(if (viewModel.showNavBar) 1f else 0f)
+                val navBarHeightFactor by animateFloatAsState(if (showNavBar) 1f else 0f)
                 SubcomposeLayout { constraints ->
                     val insetsBottom = insets.getBottom(this)
 
                     val navBar = subcompose("navBar") {
                         NavigationBar(
+                            modifier = Modifier
+                                .hazeSource(zIndex = HazeZIndex.bottomBar)
+                                .hazeEffectBottom(),
+                            containerColor = Color.Transparent,
+                            contentColor = MaterialTheme.colorScheme.onSurface,
+                            tonalElevation = 0.dp,
                             windowInsets = insets.only { horizontal + bottom }
                         ) {
                             Main.pages.forEach { page ->
                                 NavigationBarItem(
-                                    selected = viewModel.navBarEntry == page,
-                                    onClick = {
-                                        viewModel.navigateMain(page)
-                                    },
+                                    selected = navBarEntry == page,
+                                    onClick = { navigateMain(page) },
                                     icon = {
                                         Icon(page.navigationIcon, contentDescription = null)
                                     },
@@ -92,12 +106,27 @@ class MainActivity : ComponentActivity() {
                     val contentPadding = PaddingValues(bottom = paddingBottom.toDp())
 
                     val contentPlaceable = subcompose("content") {
-                        NavDisplay(
-                            backStack = viewModel.backStack,
+                        Scaffold(
                             modifier = Modifier.consumeWindowInsets(insets.only { bottom }),
-                            sceneStrategy = rememberListDetailSceneStrategy(),
-                            entryProvider = { it.navEntry(viewModel, contentPadding) },
-                        )
+                            snackbarHost = {
+                                SnackbarHost(snackbarHostState) {
+//                                    HazeSnackbar(
+                                    Snackbar(
+                                        snackbarData = it,
+                                        modifier = Modifier.hazeSource(zIndex = HazeZIndex.bottomBar),
+                                    )
+                                }
+                            },
+                            contentWindowInsets = insets + contentPadding,
+                        ) { contentPadding ->
+                            NavDisplay(
+                                backStack = backStack,
+                                modifier = Modifier.hazeSource(zIndex = HazeZIndex.navDisplay),
+                                sceneStrategy = rememberListDetailSceneStrategy(),
+                                entryProvider = { it.navEntry(contentPadding) },
+                            )
+                            Text(contentPadding.toString())
+                        }
                     }.first().measure(constraints)
 
                     layout(constraints.maxWidth, constraints.maxHeight) {
