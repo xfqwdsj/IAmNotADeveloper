8ff0edb1b81faa8a77e134dba95eaa5c615add80
LTFan <xfqwdsj@qq.com>
2025-08-12 22:33:47 +0800

    feat: Haze实用函数

---
+++ app/src/main/kotlin/ui/util/Haze.kt
@@ -0,0 +1,81 @@
+package top.ltfan.notdeveloper.ui.util
+
+import androidx.compose.ui.Modifier
+import dev.chrisbanes.haze.ExperimentalHazeApi
+import dev.chrisbanes.haze.HazeEffectScope
+import dev.chrisbanes.haze.HazeProgressive
+import dev.chrisbanes.haze.HazeState
+import dev.chrisbanes.haze.HazeStyle
+import dev.chrisbanes.haze.hazeEffect
+import dev.chrisbanes.haze.hazeSource
+import top.ltfan.notdeveloper.ui.page.Page
+import top.ltfan.notdeveloper.ui.viewmodel.AppViewModel
+
+context(viewModel: AppViewModel)
+fun Modifier.hazeSource(
+    state: HazeState = viewModel.hazeState, zIndex: Float = 0f, key: Any? = null
+) = hazeSource(state, zIndex, key)
+
+context(viewModel: AppViewModel)
+fun Modifier.hazeEffect(
+    state: HazeState = viewModel.hazeState,
+    style: HazeStyle = HazeStyle.Unspecified,
+    block: (HazeEffectScope.() -> Unit)? = null,
+) = hazeEffect(state, style, block)
+
+context(viewModel: AppViewModel)
+fun Modifier.hazeEffectTop(
+    state: HazeState = viewModel.hazeState,
+    style: HazeStyle = HazeStyle.Unspecified,
+    block: (HazeEffectScope.() -> Unit)? = null,
+) = hazeEffect(state, style) {
+    progressive = HazeProgressive.verticalGradient(
+        startY = Float.POSITIVE_INFINITY,
+        endY = 0f,
+    )
+    block?.invoke(this)
+}
+
+context(viewModel: AppViewModel)
+fun Modifier.hazeEffectBottom(
+    state: HazeState = viewModel.hazeState,
+    style: HazeStyle = HazeStyle.Unspecified,
+    block: (HazeEffectScope.() -> Unit)? = null,
+) = hazeEffect(state, style) {
+    progressive = HazeProgressive.verticalGradient()
+    block?.invoke(this)
+}
+
+context(viewModel: AppViewModel, page: Page)
+fun Modifier.contentHazeSource(state: HazeState = viewModel.hazeState) = hazeSource(
+    state = state,
+    zIndex = HazeZIndex.content,
+    key = page,
+)
+
+@OptIn(ExperimentalHazeApi::class)
+context(viewModel: AppViewModel, page: Page)
+fun Modifier.appBarHazeEffect(
+    state: HazeState = viewModel.hazeState,
+    style: HazeStyle = HazeStyle.Unspecified,
+    block: (HazeEffectScope.() -> Unit)? = null,
+) = hazeEffectTop(state, style) {
+    canDrawArea = { area ->
+        area.key == page
+    }
+    block?.invoke(this)
+}
+
+object HazeZIndex {
+    private var currentNegative = -1f
+        get() = field.also { field -= 1f }
+
+    private var currentNotNegative = 0f
+        get() = field.also { field += 1f }
+
+    val content = currentNotNegative
+    val topBar = currentNotNegative
+    val navDisplay = currentNotNegative
+    val bottomBar = currentNotNegative
+    val app = currentNotNegative
+}
