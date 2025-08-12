83600750ffacb930d7d0431275276c27f66ace4d
LTFan <xfqwdsj@qq.com>
2025-08-12 22:33:47 +0800

    feat: Haze实用函数

---
+++ app/src/main/kotlin/ui/composable/HazeSnackbar.kt
@@ -0,0 +1,352 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.ltfan.notdeveloper.ui.composable
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SnackbarData
+import androidx.compose.material3.SnackbarDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.LastBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastFirst
+import androidx.compose.ui.util.fastFirstOrNull
+import dev.chrisbanes.haze.HazeDefaults
+import dev.chrisbanes.haze.LocalHazeStyle
+import top.ltfan.notdeveloper.ui.util.HazeZIndex
+import top.ltfan.notdeveloper.ui.util.hazeEffect
+import top.ltfan.notdeveloper.ui.util.hazeSource
+import top.ltfan.notdeveloper.ui.viewmodel.AppViewModel
+import kotlin.math.max
+import kotlin.math.min
+
+@Composable
+context(viewModel: AppViewModel)
+fun HazeSnackbar(
+    snackbarData: SnackbarData,
+    modifier: Modifier = Modifier,
+    actionOnNewLine: Boolean = false,
+    shape: Shape = SnackbarDefaults.shape,
+    containerColor: Color = SnackbarDefaults.color.copy(alpha = .7f),
+    contentColor: Color = SnackbarDefaults.contentColor,
+    actionColor: Color = SnackbarDefaults.actionColor,
+    actionContentColor: Color = SnackbarDefaults.actionContentColor,
+    dismissActionContentColor: Color = SnackbarDefaults.dismissActionContentColor,
+) {
+    val actionLabel = snackbarData.visuals.actionLabel
+    val actionComposable: (@Composable () -> Unit)? =
+        if (actionLabel != null) {
+            @Composable {
+                TextButton(
+                    colors = ButtonDefaults.textButtonColors(contentColor = actionColor),
+                    onClick = { snackbarData.performAction() },
+                    content = { Text(actionLabel) }
+                )
+            }
+        } else {
+            null
+        }
+    val dismissActionComposable: (@Composable () -> Unit)? =
+        if (snackbarData.visuals.withDismissAction) {
+            @Composable {
+                IconButton(
+                    onClick = { snackbarData.dismiss() },
+                    content = {
+                        @SuppressLint("PrivateResource") Icon(
+                            Icons.Filled.Close,
+                            contentDescription = stringResource(androidx.compose.material3.R.string.m3c_snackbar_dismiss),
+                        )
+                    }
+                )
+            }
+        } else {
+            null
+        }
+    HazeSnackbar(
+        modifier = modifier.padding(12.dp),
+        action = actionComposable,
+        dismissAction = dismissActionComposable,
+        actionOnNewLine = actionOnNewLine,
+        shape = shape,
+        containerColor = containerColor,
+        contentColor = contentColor,
+        actionContentColor = actionContentColor,
+        dismissActionContentColor = dismissActionContentColor,
+        content = { Text(snackbarData.visuals.message) }
+    )
+}
+
+@Composable
+context(viewModel: AppViewModel)
+fun HazeSnackbar(
+    modifier: Modifier = Modifier,
+    action: @Composable (() -> Unit)? = null,
+    dismissAction: @Composable (() -> Unit)? = null,
+    actionOnNewLine: Boolean = false,
+    shape: Shape = SnackbarDefaults.shape,
+    containerColor: Color = SnackbarDefaults.color.copy(alpha = .7f),
+    contentColor: Color = SnackbarDefaults.contentColor,
+    actionContentColor: Color = SnackbarDefaults.actionContentColor,
+    dismissActionContentColor: Color = SnackbarDefaults.dismissActionContentColor,
+    content: @Composable () -> Unit
+) {
+    Surface(
+        modifier = modifier,
+        shape = shape,
+        color = Color.Transparent,
+        contentColor = contentColor,
+    ) {
+        Box(
+            Modifier
+                .hazeEffect(
+                    style = LocalHazeStyle.current.copy(
+                        backgroundColor = containerColor,
+                        tints = listOf(HazeDefaults.tint(containerColor)),
+                    )
+                )
+        ) {
+            val textStyle = MaterialTheme.typography.bodyMedium
+            val actionTextStyle = MaterialTheme.typography.labelLarge
+            CompositionLocalProvider(LocalTextStyle provides textStyle) {
+                when {
+                    actionOnNewLine && action != null ->
+                        NewLineButtonSnackbar(
+                            text = content,
+                            action = action,
+                            dismissAction = dismissAction,
+                            actionTextStyle = actionTextStyle,
+                            actionContentColor = actionContentColor,
+                            dismissActionContentColor = dismissActionContentColor,
+                        )
+
+                    else ->
+                        OneRowSnackbar(
+                            text = content,
+                            action = action,
+                            dismissAction = dismissAction,
+                            actionTextStyle = actionTextStyle,
+                            actionTextColor = actionContentColor,
+                            dismissActionColor = dismissActionContentColor,
+                        )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun NewLineButtonSnackbar(
+    text: @Composable () -> Unit,
+    action: @Composable () -> Unit,
+    dismissAction: @Composable (() -> Unit)?,
+    actionTextStyle: TextStyle,
+    actionContentColor: Color,
+    dismissActionContentColor: Color
+) {
+    Column(
+        modifier =
+            Modifier
+                // Fill max width, up to ContainerMaxWidth.
+                .widthIn(max = ContainerMaxWidth)
+                .fillMaxWidth()
+                .padding(start = HorizontalSpacing, bottom = SeparateButtonExtraY)
+    ) {
+        Box(
+            Modifier
+                .paddingFromBaseline(HeightToFirstLine, LongButtonVerticalOffset)
+                .padding(end = HorizontalSpacingButtonSide)
+        ) {
+            text()
+        }
+
+        Box(
+            Modifier
+                .align(Alignment.End)
+                .padding(end = if (dismissAction == null) HorizontalSpacingButtonSide else 0.dp)
+        ) {
+            Row {
+                CompositionLocalProvider(
+                    LocalContentColor provides actionContentColor,
+                    LocalTextStyle provides actionTextStyle,
+                    content = action
+                )
+                if (dismissAction != null) {
+                    CompositionLocalProvider(
+                        LocalContentColor provides dismissActionContentColor,
+                        content = dismissAction
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun OneRowSnackbar(
+    text: @Composable () -> Unit,
+    action: @Composable (() -> Unit)?,
+    dismissAction: @Composable (() -> Unit)?,
+    actionTextStyle: TextStyle,
+    actionTextColor: Color,
+    dismissActionColor: Color
+) {
+    val textTag = "text"
+    val actionTag = "action"
+    val dismissActionTag = "dismissAction"
+    Layout(
+        {
+            Box(
+                Modifier
+                    .layoutId(textTag)
+                    .padding(vertical = SnackbarVerticalPadding)
+            ) { text() }
+            if (action != null) {
+                Box(Modifier.layoutId(actionTag)) {
+                    CompositionLocalProvider(
+                        LocalContentColor provides actionTextColor,
+                        LocalTextStyle provides actionTextStyle,
+                        content = action
+                    )
+                }
+            }
+            if (dismissAction != null) {
+                Box(Modifier.layoutId(dismissActionTag)) {
+                    CompositionLocalProvider(
+                        LocalContentColor provides dismissActionColor,
+                        content = dismissAction
+                    )
+                }
+            }
+        },
+        modifier =
+            Modifier.padding(
+                start = HorizontalSpacing,
+                end = if (dismissAction == null) HorizontalSpacingButtonSide else 0.dp
+            )
+    ) { measurables, constraints ->
+        val containerWidth = min(constraints.maxWidth, ContainerMaxWidth.roundToPx())
+        val actionButtonPlaceable =
+            measurables.fastFirstOrNull { it.layoutId == actionTag }?.measure(constraints)
+        val dismissButtonPlaceable =
+            measurables.fastFirstOrNull { it.layoutId == dismissActionTag }?.measure(constraints)
+        val actionButtonWidth = actionButtonPlaceable?.width ?: 0
+        val actionButtonHeight = actionButtonPlaceable?.height ?: 0
+        val dismissButtonWidth = dismissButtonPlaceable?.width ?: 0
+        val dismissButtonHeight = dismissButtonPlaceable?.height ?: 0
+        val extraSpacingWidth = if (dismissButtonWidth == 0) TextEndExtraSpacing.roundToPx() else 0
+        val textMaxWidth =
+            (containerWidth - actionButtonWidth - dismissButtonWidth - extraSpacingWidth)
+                .coerceAtLeast(constraints.minWidth)
+        val textPlaceable =
+            measurables
+                .fastFirst { it.layoutId == textTag }
+                .measure(constraints.copy(minHeight = 0, maxWidth = textMaxWidth))
+
+        val firstTextBaseline = textPlaceable[FirstBaseline]
+        val lastTextBaseline = textPlaceable[LastBaseline]
+        val hasText =
+            firstTextBaseline != AlignmentLine.Unspecified &&
+                    lastTextBaseline != AlignmentLine.Unspecified
+        val isOneLine = firstTextBaseline == lastTextBaseline || !hasText
+        val dismissButtonPlaceX = containerWidth - dismissButtonWidth
+        val actionButtonPlaceX = dismissButtonPlaceX - actionButtonWidth
+
+        val textPlaceY: Int
+        val containerHeight: Int
+        val actionButtonPlaceY: Int
+        if (isOneLine) {
+            val minContainerHeight = 48.0.dp.roundToPx()
+            val contentHeight = max(actionButtonHeight, dismissButtonHeight)
+            containerHeight = max(minContainerHeight, contentHeight)
+            textPlaceY = (containerHeight - textPlaceable.height) / 2
+            actionButtonPlaceY =
+                if (actionButtonPlaceable != null) {
+                    actionButtonPlaceable[FirstBaseline].let {
+                        if (it != AlignmentLine.Unspecified) {
+                            textPlaceY + firstTextBaseline - it
+                        } else {
+                            0
+                        }
+                    }
+                } else {
+                    0
+                }
+        } else {
+            val baselineOffset = HeightToFirstLine.roundToPx()
+            textPlaceY = baselineOffset - firstTextBaseline
+            val minContainerHeight = 68.0.dp.roundToPx()
+            val contentHeight = textPlaceY + textPlaceable.height
+            containerHeight = max(minContainerHeight, contentHeight)
+            actionButtonPlaceY =
+                if (actionButtonPlaceable != null) {
+                    (containerHeight - actionButtonPlaceable.height) / 2
+                } else {
+                    0
+                }
+        }
+        val dismissButtonPlaceY =
+            if (dismissButtonPlaceable != null) {
+                (containerHeight - dismissButtonPlaceable.height) / 2
+            } else {
+                0
+            }
+
+        layout(containerWidth, containerHeight) {
+            textPlaceable.placeRelative(0, textPlaceY)
+            dismissButtonPlaceable?.placeRelative(dismissButtonPlaceX, dismissButtonPlaceY)
+            actionButtonPlaceable?.placeRelative(actionButtonPlaceX, actionButtonPlaceY)
+        }
+    }
+}
+
+private val ContainerMaxWidth = 600.dp
+private val HeightToFirstLine = 30.dp
+private val HorizontalSpacing = 16.dp
+private val HorizontalSpacingButtonSide = 8.dp
+private val SeparateButtonExtraY = 2.dp
+private val SnackbarVerticalPadding = 6.dp
+private val TextEndExtraSpacing = 8.dp
+private val LongButtonVerticalOffset = 12.dp
