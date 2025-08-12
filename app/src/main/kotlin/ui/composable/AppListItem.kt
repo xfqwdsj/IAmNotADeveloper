73c282b3c9529fb35007b5f83822e76175cab864
LTFan <xfqwdsj@qq.com>
2025-08-13 00:50:47 +0800

    refactor: 查询package info

--- app/src/main/kotlin/ui/composable/AppListItem.kt
+++ app/src/main/kotlin/ui/composable/AppListItem.kt
@@ -1,8 +1,8 @@
 package top.ltfan.notdeveloper.ui.composable
 
-import android.content.pm.ApplicationInfo
-import android.os.UserHandle
-import androidx.compose.foundation.layout.Row
+import android.content.pm.PackageInfo
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
@@ -10,42 +10,39 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.core.content.pm.PackageInfoCompat
 import androidx.lifecycle.application
 import coil3.compose.AsyncImage
 import coil3.request.ImageRequest
 import coil3.request.crossfade
-import top.ltfan.notdeveloper.data.UserInfo
 import top.ltfan.notdeveloper.ui.util.ListItemColorsTransparent
 import top.ltfan.notdeveloper.ui.viewmodel.AppViewModel
-import kotlin.reflect.full.staticFunctions
 
 @Composable
 fun AppViewModel.AppListItem(
-    applicationInfo: ApplicationInfo,
+    packageInfo: PackageInfo,
     onClick: (() -> Unit)? = null,
 ) {
-    val userInfo = remember(applicationInfo) {
-        val uid = applicationInfo.uid
-        val getUserId = UserHandle::class.staticFunctions.find { it.name == "getUserId" }
-        val userId = getUserId?.call(uid) as Int? ?: 0
-        service?.queryUser(userId) ?: UserInfo(
-            id = userId,
-            name = null,
-            flags = 0,
-        )
+    val applicationInfo = remember(packageInfo) {
+        application.packageManager.getApplicationInfo(packageInfo.packageName, 0)
     }
 
-    val appIcon = remember(applicationInfo) {
+    val appIcon = remember(packageInfo) {
         applicationInfo.loadIcon(application.packageManager)
     }
 
     ListItem(
         headlineContent = {
-            Row {
-                Text(applicationInfo.loadLabel(application.packageManager).toString())
-                Text(userInfo.name.toString())
-                Text(userInfo.id.toString())
-            }
+            Text(applicationInfo.loadLabel(application.packageManager).toString())
+        },
+        modifier = Modifier.run {
+            onClick?.let { clickable(onClick = it) } ?: this
+        },
+        overlineContent = {
+            Text(packageInfo.packageName)
+        },
+        supportingContent = {
+            Text("${packageInfo.versionName} (${PackageInfoCompat.getLongVersionCode(packageInfo)})")
         },
         leadingContent = {
             AsyncImage(
@@ -54,7 +51,9 @@ fun AppViewModel.AppListItem(
                     .crossfade(true)
                     .build(),
                 contentDescription = null,
-                modifier = Modifier.size(64.dp)
+                modifier = Modifier
+                    .padding(6.dp)
+                    .size(58.dp),
             )
         },
         colors = ListItemColorsTransparent,
