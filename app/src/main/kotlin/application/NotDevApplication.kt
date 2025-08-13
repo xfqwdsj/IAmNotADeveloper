4ce2019c9bc6298b368743d7718255e07fd85c5f
LTFan <xfqwdsj@qq.com>
2025-08-13 10:54:53 +0800

    refactor: simplify NotDevApplication initialization and update BinderProvider usage

--- app/src/main/kotlin/application/NotDevApplication.kt
+++ app/src/main/kotlin/application/NotDevApplication.kt
@@ -5,15 +5,9 @@ import androidx.room.Room
 import top.ltfan.notdeveloper.database.PackageSettingsDatabase
 
 class NotDevApplication : Application() {
-    val database by lazy {
-        Room.databaseBuilder(
-            this,
-            PackageSettingsDatabase::class.java,
-            PackageSettingsDatabase.DATABASE_NAME,
-        ).build()
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-    }
+    val database = Room.databaseBuilder(
+        this,
+        PackageSettingsDatabase::class.java,
+        PackageSettingsDatabase.DATABASE_NAME,
+    ).build()
 }
