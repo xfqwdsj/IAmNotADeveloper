db0c6c07864221ebe17fc811c0a1c9a0b2317a79
LTFan <xfqwdsj@qq.com>
2025-08-15 14:44:07 +0800

    refactor: 简化数据库初始化

--- app/src/main/kotlin/application/NotDevApplication.kt
+++ app/src/main/kotlin/application/NotDevApplication.kt
@@ -1,15 +1,8 @@
 package top.ltfan.notdeveloper.application
 
 import android.app.Application
-import androidx.room.Room
 import top.ltfan.notdeveloper.database.PackageSettingsDatabase
 
 class NotDevApplication : Application() {
-    val database by lazy {
-        Room.databaseBuilder(
-            this,
-            PackageSettingsDatabase::class.java,
-            PackageSettingsDatabase.DATABASE_NAME,
-        ).build()
-    }
+    val database by lazy { PackageSettingsDatabase.get() }
 }
