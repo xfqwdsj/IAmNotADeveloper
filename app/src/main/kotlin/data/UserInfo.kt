3e43fd51c7046e72e016f92109c875709bbcd0c5
LTFan <xfqwdsj@qq.com>
2025-08-16 00:57:38 +0800

    chore: 优化import

--- app/src/main/kotlin/data/UserInfo.kt
+++ app/src/main/kotlin/data/UserInfo.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import kotlinx.parcelize.parcelableCreator
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import top.ltfan.notdeveloper.R
 import top.ltfan.notdeveloper.ui.viewmodel.AppViewModel
