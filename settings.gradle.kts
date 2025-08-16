ea961965c6cca4b561722da1f8f5a599a10b065c
LTFan <xfqwdsj@qq.com>
2025-08-16 11:32:37 +0800

    chore: 更新adaptive

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven("https://androidx.dev/snapshots/builds/13931012/artifacts/repository")
+        maven("https://androidx.dev/snapshots/builds/13944877/artifacts/repository")
         maven("https://api.xposed.info/")
         maven("https://maven.kr328.app/releases")
     }
