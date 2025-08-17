a03ad682a07756103dd210b2c13da38102207bef
LTFan <xfqwdsj@qq.com>
2025-08-17 11:58:11 +0800

    refactor: 移动状态位置

--- app/src/main/kotlin/ui/page/AppConfiguration.kt
+++ app/src/main/kotlin/ui/page/AppConfiguration.kt
@@ -1,19 +1,37 @@
 package top.ltfan.notdeveloper.ui.page
 
 import android.content.pm.PackageInfo
-import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.isTraversalGroup
+import androidx.compose.ui.semantics.onClick
+import androidx.compose.ui.semantics.paneTitle
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.traversalIndex
 import androidx.compose.ui.unit.dp
+import top.ltfan.notdeveloper.R
 import top.ltfan.notdeveloper.ui.util.AppWindowInsets
 import top.ltfan.notdeveloper.ui.util.contentOverlayHaze
 import top.ltfan.notdeveloper.ui.viewmodel.AppViewModel
@@ -24,29 +42,63 @@ val AppConfigurationContainerRadius = 16.dp
 @Composable
 context(
     page: Page,
-    animatedVisibilityScope: AnimatedVisibilityScope,
     sharedTransitionScope: SharedTransitionScope,
 )
 fun AppViewModel.AppConfiguration(
-    packageInfo: PackageInfo,
+    packageInfo: PackageInfo?,
     dismiss: () -> Unit,
 ) {
+    val scrim = MaterialTheme.colorScheme.scrim.copy(.2f)
+    val title = stringResource(R.string.title_apps_configuration_modal)
+    val closeDescription = stringResource(R.string.action_apps_configuration_modal_close)
     with(sharedTransitionScope) {
-        Column(
-            modifier = Modifier
-                .padding(AppWindowInsets.asPaddingValues())
-                .padding(64.dp)
-                .sharedBounds(
-                    sharedContentState = rememberSharedContentState(AppConfigurationSharedKey),
-                    animatedVisibilityScope = animatedVisibilityScope,
-                    resizeMode = SharedTransitionScope.ResizeMode.RemeasureToBounds,
-                )
-                .clip(MaterialTheme.shapes.medium)
-                .contentOverlayHaze()
-                .clickable(onClick = dismiss)
-                .fillMaxSize(),
-        ) {
-
+        AnimatedContent(
+            targetState = packageInfo,
+            transitionSpec = { fadeIn() togetherWith fadeOut() using null },
+        ) { packageInfo ->
+            if (packageInfo != null) {
+                Box(
+                    Modifier
+                        .fillMaxSize()
+                        .background(scrim)
+                        .semantics { isTraversalGroup = true },
+                ) {
+                    Spacer(
+                        Modifier
+                            .matchParentSize()
+                            .pointerInput(dismiss) { detectTapGestures { dismiss() } }
+                            .semantics(mergeDescendants = true) {
+                                traversalIndex = 1f
+                                contentDescription = closeDescription
+                                onClick {
+                                    dismiss()
+                                    true
+                                }
+                            }
+                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(AppWindowInsets.asPaddingValues())
+                            .padding(64.dp)
+                            .sharedBounds(
+                                sharedContentState = rememberSharedContentState(
+                                    AppConfigurationSharedKey
+                                ),
+                                animatedVisibilityScope = this@AnimatedContent,
+                                resizeMode = SharedTransitionScope.ResizeMode.RemeasureToBounds,
+                            )
+                            .size(200.dp)
+                            .clip(RoundedCornerShape(AppConfigurationContainerRadius))
+                            .contentOverlayHaze()
+                            .semantics {
+                                paneTitle = title
+                                traversalIndex = 0f
+                            },
+                    ) {
+                        Text("1")
+                    }
+                }
+            }
         }
     }
 }
