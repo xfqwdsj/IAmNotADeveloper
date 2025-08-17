5c7b22a01554eae7fc85d72be59c277fbca85e25
LTFan <xfqwdsj@qq.com>
2025-08-17 21:55:34 +0800

    feat: 反射获得uid相关方法

--- app/src/main/kotlin/util/SystemUtil.kt
+++ app/src/main/kotlin/util/SystemUtil.kt
@@ -1,7 +1,9 @@
 package top.ltfan.notdeveloper.util
 
 import android.annotation.SuppressLint
+import android.os.UserHandle
 import kotlin.reflect.full.declaredFunctions
+import kotlin.reflect.full.staticFunctions
 
 val isMiui: Boolean
     @SuppressLint("PrivateApi") get() {
@@ -10,3 +12,13 @@ val isMiui: Boolean
             clazz.declaredFunctions.firstOrNull { it.name == "get" && it.parameters.size == 1 }
         return method?.call("ro.miui.ui.version.name") != ""
     }
+
+fun getUserId(uid: Int): Int {
+    val function = UserHandle::class.staticFunctions.first { it.name == "getUserId" }
+    return function.call(uid) as Int
+}
+
+fun getAppId(uid: Int): Int {
+    val function = UserHandle::class.staticFunctions.first { it.name == "getAppId" }
+    return function.call(uid) as Int
+}
