27111d1caf950835b4056083aa0cbb5afe4dd3d3
LTFan <xfqwdsj@qq.com>
2025-08-19 00:32:45 +0800

    refactor: 将原PreferenceItem重命名为DetectionItem，构造新的通用PreferenceItem

--- app/src/main/kotlin/ui/composable/PreferenceItem.kt
+++ app/src/main/kotlin/ui/composable/PreferenceItem.kt
@@ -1,67 +1,46 @@
 package top.ltfan.notdeveloper.ui.composable
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.selection.toggleable
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.CheckCircle
-import androidx.compose.material.icons.outlined.Error
-import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemColors
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.Switch
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
-import top.ltfan.notdeveloper.R
-import top.ltfan.notdeveloper.ui.util.ListItemColorsTransparent
+import androidx.compose.ui.unit.Dp
 
 @Composable
 fun PreferenceItem(
-    @StringRes nameId: Int,
-    testResult: Boolean,
-    checked: Boolean,
+    value: Boolean,
     onValueChange: (Boolean) -> Unit,
-    enabled: Boolean,
+    headlineContent: @Composable (() -> Unit),
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    overlineContent: @Composable (() -> Unit)? = null,
+    supportingContent: @Composable (() -> Unit)? = null,
+    leadingContent: @Composable (() -> Unit)? = null,
+    colors: ListItemColors = ListItemDefaults.colors(),
+    tonalElevation: Dp = ListItemDefaults.Elevation,
+    shadowElevation: Dp = ListItemDefaults.Elevation,
 ) {
-    val description = if (testResult) {
-        stringResource(R.string.test_result_on)
-    } else {
-        stringResource(R.string.test_result_off)
-    }
-
     ListItem(
-        headlineContent = {
-            Text(stringResource(nameId))
-        },
-        modifier = Modifier
+        headlineContent = headlineContent,
+        modifier = modifier
             .toggleable(
-                value = checked,
+                value = value,
                 enabled = enabled,
                 role = Role.Switch,
                 onValueChange = onValueChange,
-            )
-            .semantics {
-                contentDescription = description
-            },
-        leadingContent = {
-            if (testResult) {
-                Icon(
-                    Icons.Outlined.Error,
-                    contentDescription = null,
-                )
-            } else {
-                Icon(
-                    Icons.Outlined.CheckCircle,
-                    contentDescription = null,
-                )
-            }
-        },
+            ),
+        overlineContent = overlineContent,
+        supportingContent = supportingContent,
+        leadingContent = leadingContent,
         trailingContent = {
-            Switch(checked = checked, onCheckedChange = null, enabled = enabled)
+            Switch(checked = value, onCheckedChange = null, enabled = enabled)
         },
-        colors = ListItemColorsTransparent,
+        colors = colors,
+        tonalElevation = tonalElevation,
+        shadowElevation = shadowElevation,
     )
 }
