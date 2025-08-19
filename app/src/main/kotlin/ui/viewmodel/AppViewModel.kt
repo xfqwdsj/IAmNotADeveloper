0c54643669c9f1d866e493d1f4002b0cc9bcb592
LTFan <xfqwdsj@qq.com>
2025-08-20 02:08:09 +0800

    refactor: 用flow重构应用列表查询
    同时实现了列表下拉刷新，减少了切换到应用列表时提示框对用户的打扰

--- app/src/main/kotlin/ui/viewmodel/AppViewModel.kt
+++ app/src/main/kotlin/ui/viewmodel/AppViewModel.kt
@@ -3,22 +3,36 @@ package top.ltfan.notdeveloper.ui.viewmodel
 import android.content.Context
 import androidx.compose.animation.core.SeekableTransitionState
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewModelScope
 import dev.chrisbanes.haze.HazeState
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.ExperimentalForInheritanceCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import top.ltfan.notdeveloper.BuildConfig
 import top.ltfan.notdeveloper.application.NotDevApplication
 import top.ltfan.notdeveloper.data.PackageInfoWrapper
 import top.ltfan.notdeveloper.data.UserInfo
 import top.ltfan.notdeveloper.data.wrapped
+import top.ltfan.notdeveloper.datastore.AppFilter
 import top.ltfan.notdeveloper.datastore.AppListSettings
 import top.ltfan.notdeveloper.datastore.GlobalPreferences
 import top.ltfan.notdeveloper.datastore.model.AppDataStore
@@ -28,6 +42,7 @@ import top.ltfan.notdeveloper.log.Log
 import top.ltfan.notdeveloper.service.SystemServiceClient
 import top.ltfan.notdeveloper.service.systemService
 import top.ltfan.notdeveloper.ui.page.Apps
+import top.ltfan.notdeveloper.ui.page.Apps.processed
 import top.ltfan.notdeveloper.ui.page.Main
 import top.ltfan.notdeveloper.ui.page.Overview
 import top.ltfan.notdeveloper.ui.page.Page
@@ -71,30 +86,114 @@ class AppViewModel(app: NotDevApplication) : AndroidViewModel<NotDevApplication>
     private var _users by mutableStateOf(queryUsers())
     val users get() = _users
 
-    var selectedUser by settingsStore.propertyAsState(
+    val selectedUserFlow = settingsStore.propertyAsFlow(
         get = { it.selectedUser },
         set = { settings, user ->
-            updateAppList(user)
             settings.copy(selectedUser = user)
         },
     )
 
-    var appSortMethod by settingsStore.propertyAsState(
+    var selectedUser by selectedUserFlow.collectAsStateVM()
+
+    val appSortMethodFlow = settingsStore.propertyAsFlow(
         get = { it.sort },
         set = { settings, sort -> settings.copy(sort = sort) },
     )
 
-    var appFilteredMethods by settingsStore.propertyAsState(
+    var appSortMethod by appSortMethodFlow.collectAsStateVM()
+
+    val appFilteredMethodsFlow = settingsStore.propertyAsFlow(
         get = { it.filtered },
         set = { settings, filters -> settings.copy(filtered = filters) },
     )
 
-    private var _appList by mutableStateOf(setOf<PackageInfoWrapper>())
-    val appList get() = _appList
-    private var _currentDatabaseListState by mutableStateOf(
-        queryDatabaseList().collectAsState(emptySet())
+    var appFilteredMethods by appFilteredMethodsFlow.collectAsStateVM()
+
+    private var _isAppListError by mutableStateOf(false)
+    val isAppListError get() = _isAppListError
+    val appListErrorSnackbarTrigger = MutableSharedFlow<Unit?>()
+
+    private var _isAppListUpdating by mutableStateOf(false)
+    val isAppListUpdating get() = _isAppListUpdating
+
+    private val appListUpdateTrigger = MutableSharedFlow<Unit>()
+    val appList = combine(selectedUserFlow, appListUpdateTrigger) { userInfo, _ ->
+        queryAppList(userInfo)
+    }.stateIn(
+        viewModelScope,
+        started = SharingStarted.Eagerly,
+        initialValue = queryAppList(selectedUserFlow.value),
     )
-    val databaseList by _currentDatabaseListState
+
+    fun queryAppList(userInfo: UserInfo = selectedUserFlow.value): Set<PackageInfoWrapper> {
+        _isAppListUpdating = true
+        val list = service?.queryApps(userInfo.id)?.ifEmpty { null }?.toSet().also {
+            if (it == null) {
+                _isAppListError = true
+                viewModelScope.launch {
+                    appListErrorSnackbarTrigger.emit(Unit)
+                }
+                Log.Android.w("Failed to query apps for user ${userInfo.id}, service may not be connected")
+            } else {
+                _isAppListError = false
+            }
+        }
+        return (list ?: setOf(myPackageInfo)).also {
+            _isAppListUpdating = false
+        }
+    }
+
+    fun updateAppList() {
+        viewModelScope.launch {
+            appListUpdateTrigger.emit(Unit)
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val databaseList = selectedUserFlow.flatMapLatest {
+        queryDatabaseList(it)
+    }.stateIn(viewModelScope, started = SharingStarted.WhileSubscribed(), initialValue = emptySet())
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val appLists = combine(
+        appList,
+        databaseList,
+        appSortMethodFlow,
+        appFilteredMethodsFlow,
+    ) { appList, databaseList, sortMethod, filteredMethods ->
+        val filters = filteredMethods.subtract(AppFilter.groupingEntries)
+        (if (AppFilter.Configured !in filteredMethods) {
+            databaseList.asSequence().processed(sortMethod, filters)
+        } else emptyList()) to (if (AppFilter.Unconfigured !in filteredMethods) {
+            appList.subtract(databaseList).asSequence().processed(sortMethod, filters)
+        } else emptyList())
+    }.let { flow ->
+        val configured = flow.mapLatest { it.first }
+        val unconfigured = flow.mapLatest { it.second }
+        configured to unconfigured
+    }
+
+    val configuredAppList = appLists.first.stateIn(
+        viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        initialValue = emptyList(),
+    )
+
+    val unconfiguredAppList = appLists.second.stateIn(
+        viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        initialValue = emptyList(),
+    )
+
+    @Composable
+    fun collectAppLists(): Pair<List<PackageInfoWrapper>, List<PackageInfoWrapper>> {
+        val configuredInitial = remember { configuredAppList.value }
+        val unconfiguredInitial = remember { unconfiguredAppList.value }
+
+        val configured by configuredAppList.collectAsState(configuredInitial)
+        val unconfigured by unconfiguredAppList.collectAsState(unconfiguredInitial)
+        return configured to unconfigured
+    }
 
     fun navigateMain(page: Main) {
         if (currentPage == page) return
@@ -170,22 +269,8 @@ class AppViewModel(app: NotDevApplication) : AndroidViewModel<NotDevApplication>
 
     fun updateUsers() {
         _users = queryUsers()
-        if (selectedUser in users) return
-        selectedUser = users.first()
-    }
-
-    fun queryAppList(userInfo: UserInfo = selectedUser): Pair<Set<PackageInfoWrapper>, Boolean> {
-        val list = service?.queryApps(userInfo.id)?.ifEmpty { null }?.toSet()
-        return (list ?: setOf(myPackageInfo)) to (list == null)
-    }
-
-    fun updateAppList(list: Set<PackageInfoWrapper>, userInfo: UserInfo = selectedUser) {
-        _appList = list
-        _currentDatabaseListState = queryDatabaseList(userInfo).collectAsState(databaseList)
-    }
-
-    fun updateAppList(userInfo: UserInfo = selectedUser) {
-        updateAppList(queryAppList(userInfo).first, userInfo)
+        if (selectedUserFlow.value in users) return
+        selectedUserFlow.value = users.first()
     }
 
     fun queryDatabaseList(userInfo: UserInfo? = null) =
@@ -197,10 +282,39 @@ class AppViewModel(app: NotDevApplication) : AndroidViewModel<NotDevApplication>
             }
         }.map { service?.queryApps(it)?.toSet() ?: it.toAndroid() }
 
-    private fun <T> Flow<T>.collectAsState(initial: T): MutableState<T> {
+    private fun <T> MutableStateFlow<T>.collectAsStateVM(): MutableState<T> {
+        val delegate = mutableStateOf(value)
+        viewModelScope.launch { collect { delegate.value = it } }
+        return object : MutableState<T> by delegate {
+            override var value: T
+                get() = delegate.value
+                set(newValue) {
+                    this@collectAsStateVM.value = newValue
+                }
+        }
+    }
+
+    private fun <T> Flow<T>.collectAsStateVM(initial: T): State<T> {
         val delegate = mutableStateOf(initial)
         viewModelScope.launch { collect { delegate.value = it } }
         return delegate
+    }
+
+    @OptIn(ExperimentalForInheritanceCoroutinesApi::class)
+    private fun <T, R> AppDataStore<T>.propertyAsFlow(
+        defaultValue: T = this.defaultValue,
+        get: (T) -> R,
+        set: (T, R) -> T,
+    ): MutableStateFlow<R> {
+        val flow = MutableStateFlow(get(defaultValue))
+
+        viewModelScope.launch {
+            flow.collect { value ->
+                updateData { set(it, value) }
+            }
+        }
+
+        return flow
     }
 
     private fun <T, R> AppDataStore<T>.propertyAsState(
@@ -208,7 +322,7 @@ class AppViewModel(app: NotDevApplication) : AndroidViewModel<NotDevApplication>
         get: (T) -> R,
         set: (T, R) -> T,
     ): MutableState<R> {
-        val delegate = data.collectAsState(defaultValue)
+        val delegate = data.collectAsStateVM(defaultValue)
         return object : MutableState<R> by mutableStateOf(get(delegate.value)) {
             override var value: R
                 get() = get(delegate.value)
