50718cb62199d47ad0a2b165cba206bb963af798
LTFan <xfqwdsj@qq.com>
2025-08-30 12:19:59 +0800

    chore: upgrade agp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.composeCompiler) apply false
