a7e31ffa36a4aca9e520c42878ca63c36fea1dc8
LTFan <xfqwdsj@qq.com>
2026-09-12 01:53:58 +0800

    feat: centre the overview title

--- app/src/main/kotlin/ui/page/Overview.kt
+++ app/src/main/kotlin/ui/page/Overview.kt
@@ -6,8 +6,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -59,7 +59,7 @@ object Overview : Main() {
         Scaffold(
             modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
             topBar = {
-                LargeTopAppBar(
+                CenterAlignedTopAppBar(
                     title = {
                         Text(stringResource(R.string.app_name))
                     },
